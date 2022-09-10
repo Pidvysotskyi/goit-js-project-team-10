dcd5490d5138713e3969f9e416e01d4056add2c6
Pidvysotskyi <100088935+Pidvysotskyi@users.noreply.github.com>
2022-09-10 09:00:08 +0000

    Deploying to gh-pages from @ Pidvysotskyi/goit-js-project-team-10@c14dc195afe7e9a7b2d4da63528d1d50f41c1237 🚀

---
+++ library.2a0cda39.js
@@ -0,0 +1,2 @@
+document.querySelector(".watched_btn"),document.querySelector(".queue_btn"),document.querySelector(".footer__link");
+//# sourceMappingURL=library.2a0cda39.js.map
---
+++ library.c9142efc.js
@@ -0,0 +1,2 @@
+document.querySelector(".watched_btn"),document.querySelector(".queue_btn"),document.querySelector(".footer__link");
+//# sourceMappingURL=library.c9142efc.js.map
